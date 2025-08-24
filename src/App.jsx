2efb407c68edd0b7d3230c3592d9887e320d24cf
Stanley Pan <imoment.developer@gmail.com>
2025-08-25 04:10:39 +0900

    Project creation, initial environment setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="flex flex-col justify-center items-center text-green-600">
+      <div className="font-playfair text-3xl">React Capstone Project</div>
+      <div className="font-lato">Initial setup</div>
+      <div>Completed</div>
+    </div>
+  );
+};
+
+export default App;
