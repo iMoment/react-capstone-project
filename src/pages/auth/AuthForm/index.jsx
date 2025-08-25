0a08129cbfd2aac191d12c0aca8827035bd255d3
Stanley Pan <imoment.developer@gmail.com>
2025-08-26 04:15:22 +0900

    Add apiService

--- src/pages/auth/AuthForm/index.jsx
+++ src/pages/auth/AuthForm/index.jsx
@@ -11,8 +11,6 @@ const AuthForm = (props) => {
     return initialState;
   });
 
-  console.log(values);
-
   return (
     <form className="m-4 rounded-lg border border-slate-200 bg-white p-4 font-lato">
       {fields.map((field) => (
